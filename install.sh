d67750a9995e610b6d9a3351a3cdb932ce30fdb0
guoqing.ge <Guoqing.Ge@noaa.gov>
2025-05-09 19:50:18 -0600

    add .ssh/config and update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,9 @@ mv .bash_profile   backup_bash_${seconds}
 mv .bashrc  backup_bash_${seconds}
 mv .vimrc   backup_bash_${seconds}
 mv .tmux.conf   backup_bash_${seconds}
+mv .ssh/config   backup_bash_${seconds}
 
 cp ${srcdir}/.bash* .
 cp ${srcdir}/.vimrc .
 cp ${srcdir}/.tmux.conf .
+cp ${srcdir}/config .ssh
